9c7b70cc10bd4f60f2fc9883675d6026f24f9108
Zairus Bermillo <166342742+notZairus@users.noreply.github.com>
2025-06-22 17:58:39 +0800

    finished projects page. (i think)

--- src/components/DesktopNavigation.tsx
+++ src/components/DesktopNavigation.tsx
@@ -2,7 +2,7 @@ import NavButton from "./NavButton"
 
 export default function DesktopNavigation() {
   return (
-    <nav className='hidden lg:block md:block min-w-20 min-h-5 bg-white/15 space-x-12 px-12 py-2 rounded-lg text-white font-semibold'>
+    <nav className='hidden lg:block md:block min-w-20 min-h-5 bg-[#2F2F2F] border border-white/15 space-x-12 px-12 py-2 rounded-lg text-white font-semibold'>
         <NavButton path="/">Home</NavButton>
         <NavButton path="/about">About</NavButton>
         <NavButton path="/projects">Projects</NavButton>
--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -1,6 +1,7 @@
 import { motion } from "motion/react";
 // import type { Variants } from "motion/react"
 import Dot from "../components/Dot";
+import { cn } from "@/lib/utils";
 
 
 // const InViewVariant: Variants = {
@@ -32,28 +33,49 @@ export default function Projects() {
                 <p className='border-l-12 border-blue-400 text-xl lg:w-[800px] pl-2 mt-8'>Here are my projects that showcase my ability to build beautiful and functional websites, and reflect the effort I put into improving with every creation.</p>
             </motion.div>
 
-            <section className="mt-8 flex flex-wrap gap-8 items-start align-center">
-                <div className="shrink-0 grow bg-red-400 h-20 w-1/3 rounded-lg">
-                    
-                </div>
-
-                <div className="shrink-0 grow bg-red-400 h-20 w-1/3 rounded-lg">
-                    
-                </div>
-
-                <div className="shrink-0 grow bg-red-400 h-20 w-1/3 rounded-lg">
-                    
-                </div>
-
-
-                <div className="shrink-0 grow bg-red-400 h-20 w-1/3 rounded-lg">
-                    
-                </div>
-
-                <div className="shrink-0 grow bg-red-400 h-20 w-1/3 rounded-lg">
-                    
-                </div>
-
+            <section className="grid grid-cols-1 md:grid-cols-2 auto-rows-[100px] md:gap-x-10 gap-y-10 mt-20">
+                <div className="col-start-1 row-span-1"></div>
+                {
+                    ['Quizzital', 'FoodIfAI', 'Adam\'s Ice Cream POS', 'Random', 'Random', 'Random'].map((project, index) => (
+                        <div className={cn("bg-white/15 rounded-xl md:row-span-4 row-span-3 overflow-hidden relative w-full", index % 2 === 0 ? "col-start-2" : "col-start-1")}>
+                            <motion.img 
+                                src="https://imgs.search.brave.com/9XHXpZoWrWDCRXvqC6S26W_suvveaHy-AuGhZP_7nk8/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly93d3cu/c3F1YXNoLmlvL3N0/YXRpYy9pbWFnZXMv/dHV0b3JpYXNfZGVm/YXVsdF9pbWFnZS5q/cGc" 
+                                alt="project-preview" 
+                                className="w-full h-full object-cover"
+                                whileHover={{
+                                    scale: 1.05
+                                }}
+                            />
+                            <motion.div 
+                                className="absolute inset-0 w-full bg-gradient-to-t from-black/85 to-transparent opacity-1"
+                                whileHover={{
+                                    opacity: 1
+                                }}
+                            >
+                                <div className="absolute bottom-4 w-full px-4 py-3">
+                                    <p className="text-2xl font-semibold max-w-full break-words">
+                                        {project}
+                                        {project}
+                                    </p>
+                                    <p className="mt-2 max-w-full break-words overflow-ellipsis">
+                                        {project}
+                                        {project}
+                                        {project}
+                                        {project}
+                                        {project}
+                                        {project}
+                                        {project}
+                                        {project}
+                                        {project}
+                                        {project}
+                                        {project}
+                                        {project}
+                                    </p>
+                                </div>
+                            </motion.div>
+                        </div>
+                    ))
+                }
             </section>
 
 
