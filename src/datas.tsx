0b7c016f6b61872de760dc7e2b02489b908b42cc
Zairus Bermillo <166342742+notZairus@users.noreply.github.com>
2025-06-24 22:03:29 +0800

    fix broken images

--- src/datas.tsx
+++ src/datas.tsx
@@ -13,7 +13,7 @@ export const projects: Project[] = [
         description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta quaerat necessitatibus ullam, minima quod consectetur doloribus, error tempore repellat facilis culpa suscipit, inventore aliquam consequatur corporis libero animi optio incidunt.',
         technologyUsed: ['Html', 'PHP', 'CSS', 'Javascript', 'MySQL'],
         screenshotPaths: Array.from({ length: 8 }).map((_, index) => {
-            return `/src/assets/project_screenshots/aic_pos/_${index+1}.png`;
+            return `@/assets/project_screenshots/aic_pos/_${index+1}.png`;
         })
     },
     {
@@ -24,5 +24,5 @@ export const projects: Project[] = [
             return `/src/assets/project_screenshots/quizzital/_${index+1}.png`;
         })
     },
-    
+
 ]
