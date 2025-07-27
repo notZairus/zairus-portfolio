f0ee415415820b65f9d65f6fc29c67f8046d4794
Zairus Bermillo <166342742+notZairus@users.noreply.github.com>
2025-07-27 11:21:33 +0800

    feature(ui): create the header logo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,8 +6,8 @@ export default function Header() {
   return (
     <header className='w-full min-h-10 flex justify-between px-8 py-4 items-center fixed top-0 left-0 right-0 z-20'>
         <Link to="/">
-          <div className='aspect-square w-12 bg-white rounded-full overflow-hidden'>
-            <img src="/zairus.jpg" alt="" />
+          <div className='aspect-square flex items-center justify-center w-12 rounded-full overflow-hidden'>
+            <h1 className="text-blue-400 font-black text-2xl"><span className="text-white">Z</span>B</h1>
           </div>      
         </Link>  
         
